d58159a688d3c05e4225838e96087c38ffd43ef9
ahmadshihab <ahmad.shihab2019@gmail.com>
2021-01-26 17:43:52 +0200

    CustomDimensions

--- lib/core/custom_dimensions.dart
+++ lib/core/custom_dimensions.dart
@@ -4,7 +4,14 @@ class CustomDimensions {
   var ctx;
   double width;
   double height;
+  double devicePixelRatio;
+  double blockSizeHorizontal;
+  double blockSizeVertical;
+
   CustomDimensions(this.ctx)
       : this.width = MediaQuery.of(ctx).size.width,
-        this.height = MediaQuery.of(ctx).size.height;
+        this.height = MediaQuery.of(ctx).size.height,
+        this.devicePixelRatio = MediaQuery.of(ctx).devicePixelRatio,
+        this.blockSizeVertical = MediaQuery.of(ctx).size.height / 100,
+        this.blockSizeHorizontal = MediaQuery.of(ctx).size.width / 100;
 }
