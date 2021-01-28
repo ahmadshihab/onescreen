0f9e5063daac242efec9e108f40d322278c99d2c
ASUS <Mhd.Nofal@itland-sy.com>
2021-01-28 19:58:53 +0200

    fixing bugs

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -26,7 +26,7 @@ class App extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: StartPage(),
+      home: SongPage(),
     );
   }
 }
